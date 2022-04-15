0e00bfc44fd1623a6fc3bd745543f7500f7fc3ae
rofe-dl <rafidul258@gmail.com>
2022-04-16 04:27:16 +0600

    changed font and used one img

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-let images = [
-    'img/bg0.gif', 
-    'img/bg1.gif',
-    'img/bg2.gif',
-    'img/bg3.gif',
-    'img/bg4.gif',
-    'img/bg5.gif'
-];
+// let images = [
+//     'img/bg0.gif', 
+//     'img/bg1.gif',
+//     'img/bg2.gif',
+//     'img/bg3.gif',
+//     'img/bg4.gif',
+//     'img/bg5.gif'
+// ];
 
-function slideImage(i){
-    i = parseInt(i);
-    document.body.style.backgroundImage = `url(${images[i]})`;
-}
+// function slideImage(i){
+//     i = parseInt(i);
+//     document.body.style.backgroundImage = `url(${images[i]})`;
+// }
 
-window.onload = () => {
-    buttons = document.getElementsByClassName('nav-button');
-    for (var j = 0; j < buttons.length; j++){
-        buttons[j].addEventListener("click", (event) => {
-            slideImage(event.currentTarget.dataset.imageid);
-        })
-    }
-}
+// window.onload = () => {
+//     buttons = document.getElementsByClassName('nav-button');
+//     for (var j = 0; j < buttons.length; j++){
+//         buttons[j].addEventListener("click", (event) => {
+//             slideImage(event.currentTarget.dataset.imageid);
+//         })
+//     }
+// }
