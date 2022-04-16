fe52556303564d1ec0dac6a3c31b28319256a76e
rofe-dl <rafidul258@gmail.com>
2022-04-16 23:06:43 +0600

    bg images and splide working

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // let images = [
-//     'img/bg0.gif', 
-//     'img/bg1.gif',
+//     'img/bg1.gif', 
 //     'img/bg2.gif',
 //     'img/bg3.gif',
 //     'img/bg4.gif',
-//     'img/bg5.gif'
+//     'img/bg5.gif',
+//     'img/bg6.gif'
 // ];
 
 // function slideImage(i){
@@ -12,11 +12,43 @@
 //     document.body.style.backgroundImage = `url(${images[i]})`;
 // }
 
-// window.onload = () => {
-//     buttons = document.getElementsByClassName('nav-button');
-//     for (var j = 0; j < buttons.length; j++){
-//         buttons[j].addEventListener("click", (event) => {
-//             slideImage(event.currentTarget.dataset.imageid);
-//         })
-//     }
-// }
+// const { Controller, Move } = splide.Components;
+
+window.onload = () => {
+    var splide = new Splide('.splide', {
+        'arrows': false,
+        'pagination': false
+    });
+
+    splide.mount();
+
+    buttons = document.getElementsByClassName('slide-button');
+
+    for (var i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener("click", (event) => {
+            splide.go(parseInt(event.target.dataset.slideid));
+        })
+    }
+
+    let bgArray = [
+        'bg0.gif',
+        'bg1.gif',
+        'bg2.gif',
+        'bg3.gif',
+        'bg4.gif',
+        'bg5.gif',
+        'bg6.gif',
+        'bg7.gif',
+        'bg8.gif',
+        'bg9.gif',
+        'bg10.gif',
+        'bg11.gif'];
+
+    let bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    let path = './img/';
+
+    // window.onload = () => {
+        console.log(bg);
+        document.body.style.backgroundImage = `url(${path + bg})`;
+    // }
+}
